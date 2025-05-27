5adc1066188425889090d6aa6ff9aa07d6771096
GastonValla <gastonvalla1@gmail.com>
2025-05-27 13:43:40 -0300

    calcu1

---
+++ ej1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string a;
+int b, c=0;
+do
+{
+    Console.WriteLine("Hello, World! Ingresa un numero entero positivo ahora. ");
+    a = Console.ReadLine();
+} while (!int.TryParse(a, out b)||b<=0);
+    Console.WriteLine("numero ingresado: "+b);
+    Console.WriteLine("\n PROCEDIENDO CON LA INVERSION DE DIGITOS");
+    while(b/10 > 0)
+    {
+        c = c*10 + b%10;
+        b= b/10;
+    }
+    c=c*10+b;
+    Console.WriteLine("\n Numero invertido: "+c);
---
+++ ej2/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string a, num1, num2;
+do
+{
+    a=console.ReadLine();
+switch (a)
+{
+    case "1";
+    {
+        console.WriteLine("\n Ingrese el primer numero: ");
+        num1 = console.ReadLine();
+        console.WriteLine("\n Ingrese el segundo numero: ");
+        num2 = console.ReadLine();
+        num2 = num1+num2;
+    }break;
+    case "2";
+    {
+        console.WriteLine("\n Ingrese el primer numero: ");
+        num1 = console.ReadLine();
+        console.WriteLine("\n Ingrese el segundo numero: ");
+        num2 = console.ReadLine();
+        num2 = num1-num2;
+    }break;
+    case "3";
+    {
+        console.WriteLine("\n Ingrese el primer numero: ");
+        num1 = console.ReadLine();
+        console.WriteLine("\n Ingrese el segundo numero: ");
+        num2 = console.ReadLine();
+        num2 = num1*num2;
+    }break;
+    case "4";
+    {
+        console.WriteLine("\n Ingrese el primer numero: ");
+        num1 = console.ReadLine();
+        console.WriteLine("\n Ingrese el segundo numero: ");
+        num2 = console.ReadLine();
+        num2 = num1/num2;
+    }break;
+
+    default:
+}
+}while(a!=0);
