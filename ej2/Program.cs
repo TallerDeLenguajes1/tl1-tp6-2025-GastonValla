7957584d4d0f9ce5c5c74a03bc80d0b91fa5fbbf
GastonValla <gastonvalla1@gmail.com>
2025-06-02 12:49:34 -0300

    Calcu1Terminada

--- ej2/Program.cs
+++ ej2/Program.cs
@@ -1,44 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-string a, num1, num2;
+int a, num1, num2;
+bool n=false;
 do
 {
-    a=console.ReadLine();
+    do{n=int.TryParse(Console.ReadLine(), out a);}while(!n);
+    n = false;
 switch (a)
-{
-    case "1";
     {
-        console.WriteLine("\n Ingrese el primer numero: ");
-        num1 = console.ReadLine();
-        console.WriteLine("\n Ingrese el segundo numero: ");
-        num2 = console.ReadLine();
-        num2 = num1+num2;
-    }break;
-    case "2";
-    {
-        console.WriteLine("\n Ingrese el primer numero: ");
-        num1 = console.ReadLine();
-        console.WriteLine("\n Ingrese el segundo numero: ");
-        num2 = console.ReadLine();
-        num2 = num1-num2;
-    }break;
-    case "3";
-    {
-        console.WriteLine("\n Ingrese el primer numero: ");
-        num1 = console.ReadLine();
-        console.WriteLine("\n Ingrese el segundo numero: ");
-        num2 = console.ReadLine();
-        num2 = num1*num2;
-    }break;
-    case "4";
-    {
-        console.WriteLine("\n Ingrese el primer numero: ");
-        num1 = console.ReadLine();
-        console.WriteLine("\n Ingrese el segundo numero: ");
-        num2 = console.ReadLine();
-        num2 = num1/num2;
-    }break;
-
-    default:
-}
+        case 1:
+            {
+                Console.WriteLine("\n --- SUMA DE ENTEROS --- \n Ingrese el primer numero: ");
+                do { n = int.TryParse(Console.ReadLine(), out num1); } while (!n);
+                Console.WriteLine("\n Ingrese el segundo numero: ");
+                do { n = int.TryParse(Console.ReadLine(), out num2); } while (!n);
+                num2 = num1 + num2;
+                Console.WriteLine("\n RESULTADO: " + num2);
+                n = false;
+            }
+            break;
+        case 2:
+            {
+                Console.WriteLine("\n --- DIFERENCIA DE ENTEROS --- \n Ingrese el primer numero: ");
+                do { n = int.TryParse(Console.ReadLine(), out num1); } while (!n);
+                Console.WriteLine("\n Ingrese el segundo numero: ");
+                do { n = int.TryParse(Console.ReadLine(), out num2); } while (!n);
+                num2 = num1 - num2;
+                Console.WriteLine("\n RESULTADO: " + num2);
+                n = false;
+            }
+            break;
+        case 3:
+            {
+                Console.WriteLine("\n --- MULTIPLICACION DE ENTEROS ---\n Ingrese el primer numero: ");
+                do { n = int.TryParse(Console.ReadLine(), out num1); } while (!n);
+                Console.WriteLine("\n Ingrese el segundo numero: ");
+                do { n = int.TryParse(Console.ReadLine(), out num2); } while (!n);
+                num2 = num1*num2;
+                Console.WriteLine("\n RESULTADO: " + num2);
+                n = false;
+            }
+            break;
+        case 4:
+            {
+                Console.WriteLine("\n--- DIVISION DE ENTEROS ---\n Ingrese el numerador: ");
+                do { n = int.TryParse(Console.ReadLine(), out num1); } while (!n);
+                Console.WriteLine("\n Ingrese el denominador: ");
+                do { n = int.TryParse(Console.ReadLine(), out num2); } while (!n);
+                num2 = num1/num2;
+                Console.WriteLine("\n RESULTADO: " + num2);
+                n = false;
+            }
+            break;
+    }
 }while(a!=0);
