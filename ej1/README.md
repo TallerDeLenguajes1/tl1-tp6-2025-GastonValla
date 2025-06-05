62a4ae5b37cdcf7a4bd0977d003edd463ee558cd
GastonValla <gastonvalla1@gmail.com>
2025-06-05 10:59:07 -0300

    Ej4Hecho

--- ej1/README.md
+++ ej1/README.md
@@ -1,1 +1,9 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/24pP-Pw_)
+
+Respuestas:
+1.  String es tipo por referencia pero se comporta como inmutable. Está en el heap pero cada vez que se cambia se crea una nueva instancia del mismo.
+
+2.  \n (salto de linea), \t (tab), \\ (barra invertida),\' (comillas), \0 (nulo), \v (tab vertical), etc...
+
+3.  Si colocas @ antes de una cadena no interpreta \n, ni \t, pero permite escribir rutas.
+Si colocas $ te permite interpretar valores de variables dentrod el string usando {}.
---
+++ ej4/Program.cs
@@ -0,0 +1,87 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World! Ingrese una cadena de texto: ");
+
+string? str1, str2, str3, str4, palabra;
+int pos;
+
+str1 = Console.ReadLine();
+
+Console.WriteLine("\n Longitud del string ingresado: " + str1.Length);
+Console.WriteLine("\n Ingrese otra cadena de texto... \n");
+str2 = Console.ReadLine();
+str3 = str1 + str2;
+Console.WriteLine("\n CONCATENACION DE CADENAS: " + str3);
+
+str4 = str3.Substring(str1.Length);
+
+Console.WriteLine("\n Segunda cadena extraida de la concatenacion: " + str4 +"\n");
+Console.WriteLine("\n Concatenacion representada por foreach: ");
+
+foreach (char i in str3)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("\n Ingrese una palabra para buscar en la concatenacion: ");
+
+palabra = Console.ReadLine();
+pos = str3.IndexOf(palabra);
+if(pos != -1){Console.WriteLine("\n La palabra se encuentra en el texto en la posicion " + pos);}
+else {Console.WriteLine("\n La palabra NO ESTA en el texto. Emoji triste");}
+
+str2 = str3.ToUpper();
+Console.WriteLine("\n Cadena en MAYUSCULAS: " +str2);
+str2 = str3.ToLower();
+Console.WriteLine("\n Cadena en minusculas: " +str2);
+
+Console.WriteLine("\n Ingrese una cadena con palabras separadas por espacio: ");
+string? entrada = Console.ReadLine();
+string[] partes;
+    partes = entrada.Split(' ');
+else { Console.WriteLine("ERROR: Linea nula ingresada"); }
+
+Console.WriteLine("\n Partes encontradas:");
+int i = 0;
+foreach (string parte in partes)
+{
+    i++;
+    Console.WriteLine(i+" "+parte);
+}
+
+Console.WriteLine("\n Ingrese una operacion simple de un operando entre dos numeros");
+entrada = Console.ReadLine();
+char separador = ' ';
+int a = 0, b = 0, c =0;
+if (entrada.Contains("+")) { partes = entrada.Split('+'); separador = '+';}
+else if (entrada.Contains("-")) { partes = entrada.Split('-'); separador = '-';}
+else if (entrada.Contains("*")) { partes = entrada.Split('*'); separador = '*';}
+else if (entrada.Contains("/")) { partes = entrada.Split('/'); separador = '/';}
+
+switch (separador)
+{
+    case '+':
+        a = int.Parse(partes[0]);
+        b = int.Parse(partes[1]);
+        c = a + b;
+        break;
+    case '-':
+        a = int.Parse(partes[0]);
+        b = int.Parse(partes[1]);
+        c = a - b;
+        break;
+
+    case '*':
+        a = int.Parse(partes[0]);
+        b = int.Parse(partes[0]);
+        c = a * b;
+        break;
+
+    case '/':
+        a = int.Parse(partes[0]);
+        b = int.Parse(partes[0]);
+        c = a / b;
+        break;
+}
+Console.WriteLine("RESULTADO DE LA OPERACION INGRESADA " + a + separador + b + " " + c);
